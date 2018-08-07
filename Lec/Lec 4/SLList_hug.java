1fa4fe93dbab5ab990f68c99450b3f74b39a9a8d
PAN <xinpan1992@gmail.com>
2018-08-06 23:13:46 -0700

    change tab to space

--- Lec/Lec 4/SLList_hug.java
+++ Lec/Lec 4/SLList_hug.java
@@ -1,96 +1,96 @@
 /** An SLList is a list of integers, which hides the terrible truth
    * of the nakedness within. */
-public class SLList_hug {	
-	private static class IntNode {
-		public int item;
-		public IntNode next;
+public class SLList_hug {   
+    private static class IntNode {
+        public int item;
+        public IntNode next;
 
-		public IntNode(int i, IntNode n) {
-			item = i;
-			next = n;
-			//System.out.println(size);
-		}
-	} 
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+            //System.out.println(size);
+        }
+    } 
 
-	/* The first item (if it exists) is at sentinel.next. */
-	private IntNode sentinel;
-	private int size;
+    /* The first item (if it exists) is at sentinel.next. */
+    private IntNode sentinel;
+    private int size;
 
-	private static void lectureQuestion() {
-		SLList_hug L = new SLList_hug();
-		IntNode n = new IntNode(5, null);
-	}
+    private static void lectureQuestion() {
+        SLList_hug L = new SLList_hug();
+        IntNode n = new IntNode(5, null);
+    }
 
-	/** Creates an empty SLList. */
-	public SLList_hug() {
-		sentinel = new IntNode(63, null);
-		size = 0;
-	}
+    /** Creates an empty SLList. */
+    public SLList_hug() {
+        sentinel = new IntNode(63, null);
+        size = 0;
+    }
 
-	public SLList_hug(int x) {
-		sentinel = new IntNode(63, null);
-		sentinel.next = new IntNode(x, null);
-		size = 1;
-	}
+    public SLList_hug(int x) {
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
+    }
 
 
- 	/** Adds x to the front of the list. */
- 	public void addFirst(int x) {
- 		sentinel.next = new IntNode(x, sentinel.next);
- 		size = size + 1;
- 	}
+    /** Adds x to the front of the list. */
+    public void addFirst(int x) {
+        sentinel.next = new IntNode(x, sentinel.next);
+        size = size + 1;
+    }
 
- 	/** Returns the first item in the list. */
- 	public int getFirst() {
- 		return sentinel.next.item;
- 	}
+    /** Returns the first item in the list. */
+    public int getFirst() {
+        return sentinel.next.item;
+    }
 
- 	/** Adds x to the end of the list. */
- 	public void addLast(int x) {
- 		size = size + 1; 		
+    /** Adds x to the end of the list. */
+    public void addLast(int x) {
+        size = size + 1;        
 
- 		IntNode p = sentinel;
+        IntNode p = sentinel;
 
- 		/* Advance p to the end of the list. */
- 		while (p.next != null) {
- 			p = p.next;
- 		}
+        /* Advance p to the end of the list. */
+        while (p.next != null) {
+            p = p.next;
+        }
 
- 		p.next = new IntNode(x, null);
- 	}
+        p.next = new IntNode(x, null);
+    }
 
- 	/** Delet the first element of the list 
- 	Make sure the three Invariants are properly maintained
- 	
- 	The sentinel reference always points to a sentinel node.
+    /** Delet the first element of the list 
+    Make sure the three Invariants are properly maintained
+    
+    The sentinel reference always points to a sentinel node.
     The front item (if it exists), is always at sentinel.next.item.
     The size variable is always the total number of items that have been added.*/
- 	public void deleteFirst(){
- 		if(sentinel.next == null) {   // empty list
- 			System.out.println("WARN : The SLList is empty!"); 
- 		} else if(sentinel.next.next == null) { // single element in the list
- 			sentinel.next = null;
- 			size--;
- 		}else {
- 			sentinel.next = sentinel.next.next;
- 			size--;
- 			}
- 	}
- 	
- 	
- 	/** Returns the size of the list. */
- 	public int size() {
- 		return size;
- 	}
+    public void deleteFirst(){
+        if(sentinel.next == null) {   // empty list
+            System.out.println("WARN : The SLList is empty!"); 
+        } else if(sentinel.next.next == null) { // single element in the list
+            sentinel.next = null;
+            size--;
+        }else {
+            sentinel.next = sentinel.next.next;
+            size--;
+            }
+    }
+    
+    
+    /** Returns the size of the list. */
+    public int size() {
+        return size;
+    }
 
-	public static void main(String[] args) {
- 		/* Creates a list of one integer, namely 10 */
- 		SLList_hug L = new SLList_hug();
- 		L.addLast(20);
- 		L.addLast(30);
- 		L.addLast(40);
- 		L.deleteFirst();
- 		System.out.println(L.size());
- 	}
+    public static void main(String[] args) {
+        /* Creates a list of one integer, namely 10 */
+        SLList_hug L = new SLList_hug();
+        L.addLast(20);
+        L.addLast(30);
+        L.addLast(40);
+        L.deleteFirst();
+        System.out.println(L.size());
+    }
 
 }
